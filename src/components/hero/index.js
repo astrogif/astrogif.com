de48a6552d1f5cb3c7bd441fed2d0e21ec62e77d
James Macfie <james@macfie.co.nz>
2017-01-19 08:17:17 +1300

    Fix incorrect link in the hero

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,6 +3,9 @@ import Button from '../button';
 import Container from '../container';
 import Sampler from '../sampler';
 import CurrentVersion from '../currentVersion';
+import { version } from '../../../package.json';
+
+const href = `https://github.com/astrogif/astrogif/releases/download/v${version}/AstroGif-${version}.dmg`;
 
 export default class Hero extends Component {
 	render() {
@@ -11,7 +14,7 @@ export default class Hero extends Component {
 				<Sampler />
 				<h1>AstroGif</h1>
 				<h2>A menubar app for finding the perfect gif</h2>
-				<Button type="alt" href="#">Download for Mac</Button>
+				<Button type="alt" href={href}>Download for Mac</Button>
 				<CurrentVersion/>
 			</Container>
 		);
