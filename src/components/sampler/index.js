7cde5eef598d259c15050716ce29cc294de3b101
James Macfie <james@macfie.co.nz>
2017-02-10 10:58:38 +1300

    Update to version 0.2.6

--- src/components/sampler/index.js
+++ src/components/sampler/index.js
@@ -4,7 +4,6 @@ import style from './style';
 
 export default class Sampler extends Component {
 	render() {
-		console.log('SAMPLR');
 		return (<div className={style.sampler}>
 			<Screen />
 			<img alt="AstroGif icon" class={style.icon} src="./assets/images/icon128.png" />
